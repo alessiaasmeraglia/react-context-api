5f633bcb93372ec20883675bbbf5e3534cabbc5b
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-15 10:13:34 +0200

    Settato variabile di contesto a null

--- src/context/BudgetContext.jsx
+++ src/context/BudgetContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 
-const BudgetContext = createContext();
+const BudgetContext = createContext(null);
 
 function BudgetProvider ({ children }){
     const [budgetMode, setBudgetMode] = useState(false);
